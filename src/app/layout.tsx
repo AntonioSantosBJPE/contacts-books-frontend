08d6413f92b49a515b4fd96f23eb4e55d028f798
Antonio <netoifpe@gmail.com>
2023-05-23 15:31:33 -0300

    inital commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Contacts Book",
+  description:
+    "Uma aplicação para facilitar o gerenciamento de sua lista de contatos",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
